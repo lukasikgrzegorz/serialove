c1a59d4acb78d8335b42daf247dcb26f6e428573
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-12-19 08:29:05 +0100

    hashrouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import App from "./App";
@@ -11,8 +11,8 @@ const root = createRoot(container);
 
 root.render(
 	<Provider store={store}>
-		<BrowserRouter>
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</Provider>
 );
